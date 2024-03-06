e0cc5bddd5222ef36f762740d1661a7ec1f91bad
Shanover <shanoversaiyed@gmail.com>
2024-03-06 02:18:42 -0500

    Pipeline to Trainable CSV code

--- client/src/Pipeline_V2D.py
+++ client/src/Pipeline_V2D.py
@@ -6,7 +6,6 @@ import json
 import pandas as pd
 from sklearn.cluster import KMeans
 
-
 class Pipeline_V2D:
     """
     Generates a pipeline for V2D:
@@ -19,7 +18,23 @@ class Pipeline_V2D:
     Step 7: Get the Entry cluster (m=0), Exit cluster (m=1) 
     Step 8: Get sequence of frames from Entry/Exit clusters and save to csv with label 'Exit' or 'Entry'
     """
+
+    def __init__(self, mp4_file_path):
+        self.mp4_file_path = mp4_file_path
+        self.mp_pose = mp.solutions.pose.Pose()
+        self.pose_processor = PoseProcessor()
+
+        self.sequence_directory = 'sequences'
+        self.trainable_dir = 'trainable_data'
+        self.columns = ['Frame', 'left_arm', 'right_arm', 'left_elbow', 'right_elbow',
+                        'left_waist_leg', 'right_waist_leg', 'left_knee', 'right_knee',
+                        'leftup_chest_inside', 'rightup_chest_inside', 'leftlow_chest_inside',
+                        'rightlow_chest_inside', 'leg_spread']
+
     def create_directories(self, directory_names):
+        """
+        Create directories if they don't exist.
+        """
         for directory_name in directory_names:
             directory_path = os.path.join(os.getcwd(), directory_name)
 
@@ -29,29 +44,24 @@ class Pipeline_V2D:
             else:
                 print(f"Directory '{directory_name}' already exists.")
 
-
-    def __init__(self, mp4_file_path):
-        self.mp4_file_path = mp4_file_path
-        self.mp_pose = mp.solutions.pose.Pose()
-        self.pose_processor = PoseProcessor()
-
-        self.sequence_directory = 'sequences'
-        self.columns = ['Frame', 'left_arm', 'right_arm', 'left_elbow', 'right_elbow',
-                'left_waist_leg', 'right_waist_leg', 'left_knee', 'right_knee',
-                'leftup_chest_inside', 'rightup_chest_inside', 'leftlow_chest_inside',
-                'rightlow_chest_inside', 'leg_spread']
-
     def getIndexOfColumn(self, column):
+        """
+        Get the index of a column in the DataFrame.
+        """
         return self.columns.index(column)
-    
+
     def save_keypoints_to_json(self, filename='keypoints.json'):
+        """
+        Save keypoints to a JSON file.
+        """
         path = f'json/{self.mp4_file_path.split("/")[-1].split(".")[0]}keypoints.json'
-        """Save keypoints to a JSON file."""
         with open(path, 'w') as json_file:
             json.dump(self.keypoints, json_file)
 
     def load_keypoints_from_json(self, filename='keypoints.json'):
-        """Load keypoints from a JSON file."""
+        """
+        Load keypoints from a JSON file.
+        """
         path = f'json/{self.mp4_file_path.split("/")[-1].split(".")[0]}keypoints.json'
         try:
             with open(os.path.join(path, filename), 'r') as json_file:
@@ -63,6 +73,9 @@ class Pipeline_V2D:
             print(f"Error: Failed to decode JSON from {os.path.join(path, filename)}. The file may be corrupted.")
 
     def separate_non_continuous_numbers(self, numbers):
+        """
+        Separate non-continuous numbers into clusters.
+        """
         clusters = []
         current_cluster = []
 
@@ -77,19 +90,22 @@ class Pipeline_V2D:
             clusters.append(current_cluster)
 
         return clusters
-    
+
     def save_sequence(self, numbers, filename):
+        """
+        Save a sequence of numbers to a file.
+        """
         full_path = os.path.join(self.sequence_directory, filename)
 
         with open(full_path, 'w') as json_file:
             json.dump(numbers, json_file)
 
         print(f'Numbers saved to {full_path}')
-    
-    # STEP METHODS
 
     def step1_load_mp4(self):
-        # Check if the MP4 file exists
+        """
+        Step 1: Load the MP4 file.
+        """
         if os.path.exists(self.mp4_file_path):
             print(f"MP4 file '{self.mp4_file_path}' exists. Loading...")
             # Add your loading logic here
@@ -97,10 +113,12 @@ class Pipeline_V2D:
             print(f"Error: MP4 file '{self.mp4_file_path}' not found. Please provide a valid file path.")
 
     def step2_extract_frames_and_create_pose(self):
+        """
+        Step 2: Extract frames and create human pose.
+        """
         cap = cv2.VideoCapture(self.mp4_file_path)
         keypoints = []
 
-        # Check if the video capture is successful
         if not cap.isOpened():
             print(f"Error: Could not open video file '{self.mp4_file_path}'.")
             return
@@ -110,103 +128,73 @@ class Pipeline_V2D:
         while True:
             ret, frame = cap.read()
 
-            # Break the loop if there are no more frames
             if not ret:
                 break
 
-            # Process the frame using OpenCV and MediaPipe
             rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             results = self.mp_pose.process(rgb_image)
 
-            # Assuming you have a pose_processor instance with a process method
             keypoints.append(self.pose_processor.process(results))
 
             frame_count += 1
 
-        # Release the video capture object
         cap.release()
         self.keypoints = keypoints
         self.save_keypoints_to_json()
         return len(keypoints)
 
-    #   NOTE: NOT REQUIRED
-    def step3_calculate_angles(self):
-        # Implementation for calculating the 13 angles
-        pass
-
     def step4_calculate_major_exercises(self):
+        """
+        Step 4: Calculate major exercises.
+        """
         df = pd.DataFrame(columns=self.columns)
 
-
         for index, row in enumerate(self.keypoints):
-            if row is None: # Check if row is None
-                continue # Skip this iteration if row is None
-            
+            if row is None:
+                continue
+
             vals = []
             vals.append(index)
             for i in row:
-                if i is None: # Check if i is None
-                    continue # Skip this iteration if i is None
+                if i is None:
+                    continue
                 val = i[1]
                 vals.append(val)
-            
-            # Create a new DataFrame for the current row
+
             new_row_df = pd.DataFrame([vals], columns=df.columns)
-            
-            # Concatenate the new DataFrame with the existing DataFrame
             df = pd.concat([df, new_row_df], ignore_index=True)
 
+        self.df = df
 
-        self.df = df # Save the DataFrame to the object
-
-        # Calculate the rolling mean for each column
-        # self.df[self.columns] = self.df[self.columns].rolling(window=90, min_periods=1, axis=0).mean()
-        # print('Columns names after rolling:', self.df.head(1))
-        # NOTE: KEEP THIS COMMENTED BECAUSE IT HAS SEVERE EFFECTS ON THE CLUSTER SEQUENCE EXTRACTION STEP 7
-        
-        # Calculate the overall deviation percentage for each column
         df_prop = df.iloc[:, 1:]
-        
-        overall_deviation_percentage = ((df_prop.max() - df_prop.min()) / df_prop.mean()) * 100
-        sorted_columns = overall_deviation_percentage.sort_values(ascending=False)
+        percentage_change = (df_prop.std() / df_prop.mean()) * 100
+        sorted_columns = percentage_change.sort_values(ascending=False)
 
         NOS_COLUMNS_TO_CONSIDER = 1
-        print(sorted_columns[:NOS_COLUMNS_TO_CONSIDER])
+        print(sorted_columns)        
         self.top_changing_columns = sorted_columns[:NOS_COLUMNS_TO_CONSIDER].index.tolist()
         return self.top_changing_columns
 
-
-    #  NOTE: NOT REQUIRED
-    def step5_calculate_peaks_and_valleys(self):
-        # Implementation for calculating peaks and valleys for each major angle and save for threshold model
-        pass
-
     def step6_kmeans_clustering(self, k=3):
+        """
+        Step 6: Perform K-means clustering.
+        """
         for col in self.top_changing_columns:
-            # Extract the column values for clustering
             column_data = self.df[col].values.reshape(-1, 1)
-
-            # Apply KMeans clustering
             kmeans = KMeans(n_clusters=k, random_state=42)
             cluster_labels = kmeans.fit_predict(column_data)
-
-            # Add cluster labels to the DataFrame
-            # cluster_column_name = f'{col}_cluster'
             cluster_column_name = f'cluster'
             self.df[cluster_column_name] = cluster_labels
 
-    def step7_get_entry_exit_clusters(self):
-        # Implementation for getting Entry cluster (m=0) and Exit cluster (m=1)
-        seq_length = 30  # You can try with 10, 20, 30, 40, 50
-
-        # Desired clusters to extract
+    def step7_get_entry_exit_clusters_and_finalize(self, create_video_clips=False):
+        """
+        Step 7: Get entry and exit clusters.
+        """
+        seq_length = 30
         entry_cluster = 0
         exit_cluster = 1
 
-        # Filter the data based on the cluster label for Entry cluster (m=0)
         entry_cluster_data = self.df[self.df['cluster'] == entry_cluster]
-
-        # Extract corresponding frames for Entry cluster
         entry_frames = entry_cluster_data['Frame'][seq_length:].tolist()
 
         entry_non_continous = self.separate_non_continuous_numbers(entry_frames)
@@ -215,15 +203,12 @@ class Pipeline_V2D:
             if len(entry_li) > seq_length:
                 entry_valid.append(entry_li)
 
-        # Save the sequences for Entry cluster
+        self.entry_sequence = entry_valid
         entry_file_prefix = 'entry_'
         entry_filename = f'{entry_file_prefix}{self.mp4_file_path.split("/")[-1].split(".")[0]}_seq.json'
         self.save_sequence(entry_valid, entry_filename)
 
-        # Filter the data based on the cluster label for Exit cluster (m=1)
         exit_cluster_data = self.df[self.df['cluster'] == exit_cluster]
-
-        # Extract corresponding frames for Exit cluster
         exit_frames = exit_cluster_data['Frame'][seq_length:].tolist()
 
         exit_non_continous = self.separate_non_continuous_numbers(exit_frames)
@@ -232,42 +217,135 @@ class Pipeline_V2D:
             if len(exit_li) > seq_length:
                 exit_valid.append(exit_li)
 
-        # Save the sequences for Exit cluster
+        self.exit_sequence = exit_valid
         exit_file_prefix = 'exit_'
         exit_filename = f'{exit_file_prefix}{self.mp4_file_path.split("/")[-1].split(".")[0]}_seq.json'
         self.save_sequence(exit_valid, exit_filename)
 
+        # Call create_video_clips function for both entry and exit frames
+        if create_video_clips:
+            input_video_path = self.mp4_file_path
+            output_folder = 'seq_clips'
+            self.create_video_clips(entry_valid, input_video_path, output_folder, subfix='entry')
+            self.create_video_clips(exit_valid, input_video_path, output_folder, subfix='exit')
 
-    def step8_save_to_csv_with_label(self):
-        # Implementation for saving sequence of frames from Entry/Exit clusters to csv with label 'Exit' or 'Entry'
-        pass
+    def step8_extract_trainable_data(self):
+
+        keypoints = self.keypoints
+        entry_frames = self.entry_sequence
+        exit_frames = self.exit_sequence
+        
+        # Extract keypoints for entry frames
+        entry_keypoints = []
+        for sequence in entry_frames:
+            for frame_number in sequence:
+                selected_keypoint = keypoints[frame_number]
+                # select keypoint is collection of 13 list of numbers like ["left_arm", 148.73277432475436], ["right_arm", 154.17960283981398], ["left_elbow", 2.040557170703907], ["right_elbow", 6.575634946201725], ["left_waist_leg", 11.75987017095535], ["right_waist_leg", 11.78409467667364], ["left_knee", 1.0046045256151428], ["right_knee", 4.645123554163573], ["leftup_chest_inside", 98.14604135044894], ["rightup_chest_inside", 98.11155727779096], ["leftlow_chest_inside", 83.67468502022136], ["rightlow_chest_inside", 89.03894876038952], ["leg_spread", 144.3427620913677]
+                # extract only the values and append to entry_keypoints
+                selected_keypoint = [i[1] for i in selected_keypoint]
+                entry_keypoints.append(selected_keypoint)
+
+        # Create a DataFrame with keypoints and a label column
+        entry_df = pd.DataFrame(entry_keypoints, columns=self.columns[1:])
+        entry_df['Label'] = 'Entry'
+
+        # Assuming you want to add a column for sequence numbers
+        entry_df['Sequence'] = [i for i, sequence in enumerate(entry_frames) for _ in sequence]
+
+        # Optionally, you can reset the index if needed
+        entry_df.reset_index(drop=True, inplace=True)
+
+        # Extract keypoints for exit frames
+        exit_keypoints = []
+        for sequence in exit_frames:
+            for frame_number in sequence:
+                selected_keypoint = keypoints[frame_number]
+                selected_keypoint = [i[1] for i in selected_keypoint]
+                exit_keypoints.append(selected_keypoint)
+
+        # Create a DataFrame with keypoints and a label column
+        exit_df = pd.DataFrame(exit_keypoints, columns=self.columns[1:])
+        exit_df['Label'] = 'Exit'
+
+        # Assuming you want to add a column for sequence numbers
+        exit_df['Sequence'] = [i for i, sequence in enumerate(exit_frames) for _ in sequence]
+
+        # Optionally, you can reset the index if needed
+        exit_df.reset_index(drop=True, inplace=True)
+
+        # Concatenate entry and exit DataFrames
+        trainable_data = pd.concat([entry_df, exit_df], ignore_index=True)
+
+        # Save the trainable data to a CSV file
+        mp4_filename = self.mp4_file_path.split("/")[-1].split(".")[0]
+        trainable_data.to_csv(os.path.join(self.trainable_dir, f'{mp4_filename}_trainable_data.csv'), index=False)
+
+        print(f"Trainable data saved to {os.path.join(self.trainable_dir, f'{mp4_filename}_trainable_data.csv')}")
+
+    # Function to create video clips from specified frames
+    def create_video_clips(self, frame_numbers, input_video_path, output_folder, subfix='clip'):
+        # Read the input video
+        video_capture = cv2.VideoCapture(input_video_path)
+
+        # Get video properties
+        frame_rate = video_capture.get(cv2.CAP_PROP_FPS)
+        frame_width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        frame_height = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        # Create output folder if it doesn't exist
+        os.makedirs(output_folder, exist_ok=True)
+
+        # Iterate through each list of frame numbers
+        for i, frame_list in enumerate(frame_numbers):
+            # Create a VideoWriter object for the output video clip
+            output_filename = f'{self.mp4_file_path.split("/")[-1].split(".")[0]}_{subfix}_{i+1}.mp4'
+            output_path = os.path.join(output_folder, output_filename)
+            video_writer = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'mp4v'), frame_rate, (frame_width, frame_height))
+
+            # Iterate through each frame number in the list
+            for frame_number in frame_list:
+                # Set the video capture to the specified frame
+                video_capture.set(cv2.CAP_PROP_POS_FRAMES, frame_number - 1)
+
+                # Read and write the frame to the output video clip
+                ret, frame = video_capture.read()
+                if ret:
+                    video_writer.write(frame)
+
+            # Release the VideoWriter object
+            video_writer.release()
+
+        # Release the VideoCapture object
+        video_capture.release()
+
 
 if __name__ == "__main__":
+    video_files = [
+        "videos/train_dumbbell_biceps_curls.mp4",
+        "videos/test_dumbbell_biceps_curls.mp4",
+        "videos/validate_dumbbell_curl_trifecta.mp4"
+    ]
 
-    
-    mp4_file_path = "videos/squat.mp4"
+    for mp4_file_path in video_files:
+        pipeline = Pipeline_V2D(mp4_file_path)
 
-    # Create an instance of the Pipeline_V2D class
-    pipeline = Pipeline_V2D(mp4_file_path)
+        pipeline.create_directories(['json', 'sequences','clips'])
+        print("Directories created.")
 
-    # Create the required directories
-    pipeline.create_directories(['json', 'sequences'])
+        pipeline.step1_load_mp4()
+        print("MP4 file loaded.")
 
-    # Step 1: Load the MP4 file
-    pipeline.step1_load_mp4()
+        points = pipeline.step2_extract_frames_and_create_pose()
+        print("Frames extracted and pose created.")
 
-    # Step 2: Extract frames and create human pose
-    points = pipeline.step2_extract_frames_and_create_pose()
+        top_angles = pipeline.step4_calculate_major_exercises()
+        print('Top angle column:', top_angles)
 
-    # Step 4: Calculate major exercises
-    top_angles = pipeline.step4_calculate_major_exercises()
-    print('Top angle column:', top_angles)
+        pipeline.step6_kmeans_clustering()
+        print("K-means clustering performed.")
 
-    # Step 6: Perform K-means clustering
-    pipeline.step6_kmeans_clustering()
+        pipeline.step7_get_entry_exit_clusters_and_finalize()
+        print("Entry and exit clusters obtained.")
 
-    # Step 7: Get entry and exit clusters
-    pipeline.step7_get_entry_exit_clusters()
-
-    # Step 8: Save sequences to CSV with labels
-    pipeline.step8_save_to_csv_with_label()
+        pipeline.step8_extract_trainable_data()
+        print("Trainable data extracted.")
